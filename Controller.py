ab8d8a486316934c8408cddf2731b2732697cac7
Simon Saqueton <ssaqueton@unomaha.edu>
2022-05-11 13:02:13 -0500

    Made controller and GUI (To_Do.py) for Project_2_main.py. I made/fixed add, del, clear methods to the controller.

---
+++ Controller.py
@@ -0,0 +1,101 @@
+import os
+import csv
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+from To_Do import *
+
+
+class Controller(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setupUi(self)
+
+        self.button_Add.clicked.connect(lambda: self.add_item())
+        self.button_Del.clicked.connect(lambda: self.del_item())
+        self.button_clear.clicked.connect(lambda: self.clear_list())
+
+        self.to_do_dict = {}
+
+        self.read_list()
+
+    def add_item(self):
+        if self.textEdit_Descripton.toPlainText() == '':
+            pass
+        else:
+            num = self.spinBox_itemNum.value()
+            if num == 0:
+                if self.listWidget.count() == 0:
+                    num = 1
+                    self.to_do_dict.update({num: self.textEdit_Descripton.toPlainText()})
+                    self.update_list()
+                else:
+                    num = len(self.to_do_dict) + 1
+                    self.to_do_dict.update({num: self.textEdit_Descripton.toPlainText()})
+                    self.update_list()
+            elif num in self.to_do_dict or num == len(self.to_do_dict) + 1:
+                temp_dict = {}
+                for key in range(num, len(self.to_do_dict) + 1):
+                    temp_dict[key + 1] = self.to_do_dict[key]
+                self.to_do_dict.update(temp_dict)
+                self.to_do_dict.update({num: self.textEdit_Descripton.toPlainText()})
+                self.update_list()
+            else:
+                self.label_error.setText(f"ERROR: Item number {str(num)}\nis not on the list")
+
+    def del_item(self):
+        try:
+            num = self.spinBox_itemNum.value()
+            if num == 0:
+                num = len(self.to_do_dict)
+                self.to_do_dict.pop(num)
+                self.update_list()
+            elif num in self.to_do_dict:
+                for key in range(num, len(self.to_do_dict)):
+                    self.to_do_dict[key] = self.to_do_dict[key + 1]
+                self.to_do_dict.pop(len(self.to_do_dict))
+                self.update_list()
+            else:
+                self.label_error.setText(f"ERROR: Item number {str(num)}\nis not on the list")
+        except KeyError:
+            self.label_error.setText(f"ERROR: There are no items\nto delete.")
+
+    def clear_list(self):
+        self.to_do_dict.clear()
+        self.listWidget.clear()
+
+    def update_list(self):
+        self.listWidget.clear()
+        self.textEdit_Descripton.clear()
+        for num in range(1, len(self.to_do_dict) + 1):
+            self.listWidget.addItem(f'{str(num)}. {self.to_do_dict[num]}')
+        self.save_list()
+        self.label_error.setText("Note:\"Add Item\"\\\"Delete\nItem\" adds\\deletes the item\nat the end of the list when\n\"Item Number\" is set to 0.")
+
+    def save_list(self):
+        with open('to_do.csv', 'w') as csvfile:
+            for key in self.to_do_dict.keys():
+                csvfile.write("%s,%s\n" % (key, self.to_do_dict[key]))
+
+    def read_list(self):
+        try:
+            with open("to_do.csv", "r") as file_dict:
+                csvFile = csv.reader(file_dict)
+                for lines in csvFile:
+                    key = lines[0]
+                    value = lines[1]
+                    self.to_do_dict.update({int(key): value})
+            self.update_list()
+        except FileNotFoundError:
+            pass
+
+
+        """self.textEdit_Descripton.keyPressEvent = self.keyPressEvent
+
+    def keyPressEvent(self, e):
+        print("event", e)
+        if e.key() == Qt.Key_Return:
+            self.add_item()
+        elif e.key() == Qt.Key_Enter:
+            self.add_item()
+        else:
+            self.textEdit_Descripton.append()"""
---
+++ Project_2_main.py
@@ -0,0 +1,13 @@
+import os
+from Controller import *
+
+def main():
+    app = QApplication([])
+    window = Controller()
+    window.show()
+    app.exec_()
+
+
+
+if __name__=='__main__':
+    main()
