b8d00a4460d98bed97e09d1ece9edf3345af2256
Simon Saqueton <ssaqueton@unomaha.edu>
2022-05-11 22:29:10 -0500

    Wrote doc strings for Controller.py and debugged method clear_list.

--- Controller.py
+++ Controller.py
@@ -1,12 +1,48 @@
-import os
 import csv
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt
 from To_Do import *
 
 
+def read_list(dict):
+    """
+    function updates dict from reading "to_do.csv" file.
+    :param dict: dictionary (self.to_do_dict)
+    """
+    try:
+        with open("to_do.csv", "r") as file_dict:
+            csvFile = csv.reader(file_dict)
+            for lines in csvFile:
+                key = lines[0]
+                value = lines[1]
+                dict.update({int(key): value})
+            for key in dict.keys():
+                if '!@#$%' in dict[key]:
+                    dict[key] = dict[key].replace('!@#$%', '\n    ')
+    except FileNotFoundError:
+        pass
+
+
+def save_list(dict):
+    """
+    function writes the contents of dict to "to_do.csv" file.
+    :param dict: dictionary (self.to_do_dict)
+    """
+    with open('to_do.csv', 'w') as csvfile:
+        for key in dict.keys():
+            if '\n    ' in dict[key]:
+                dict[key] = dict[key].replace('\n    ', '!@#$%')
+            csvfile.write("%s,%s\n" % (key, dict[key]))
+
+
 class Controller(QMainWindow, Ui_MainWindow):
+    """
+    class creates a controller to control To_Do.py GUI
+    """
+
     def __init__(self, *args, **kwargs):
+        """
+        method initializes controller class, sets up buttons, creates self.to_do_dict
+        """
         super().__init__(*args, **kwargs)
         self.setupUi(self)
 
@@ -14,11 +50,15 @@ class Controller(QMainWindow, Ui_MainWindow):
         self.button_Del.clicked.connect(lambda: self.del_item())
         self.button_clear.clicked.connect(lambda: self.clear_list())
 
+        self.temp_to_do_dict = {}
         self.to_do_dict = {}
-
-        self.read_list()
+        read_list(self.to_do_dict)
+        self.update_list()
 
     def add_item(self):
+        """
+        method adds text from self.textEdit_Descripton button to self.to_do_dict
+        """
         if self.textEdit_Descripton.toPlainText() == '':
             pass
         else:
@@ -26,23 +66,26 @@ class Controller(QMainWindow, Ui_MainWindow):
             if num == 0:
                 if self.listWidget.count() == 0:
                     num = 1
-                    self.to_do_dict.update({num: self.textEdit_Descripton.toPlainText()})
+                    self.to_do_dict.update({num: self.textEdit_Descripton.toPlainText().replace('\n', '\n    ')})
                     self.update_list()
                 else:
                     num = len(self.to_do_dict) + 1
-                    self.to_do_dict.update({num: self.textEdit_Descripton.toPlainText()})
+                    self.to_do_dict.update({num: self.textEdit_Descripton.toPlainText().replace('\n', '\n    ')})
                     self.update_list()
             elif num in self.to_do_dict or num == len(self.to_do_dict) + 1:
                 temp_dict = {}
                 for key in range(num, len(self.to_do_dict) + 1):
                     temp_dict[key + 1] = self.to_do_dict[key]
                 self.to_do_dict.update(temp_dict)
-                self.to_do_dict.update({num: self.textEdit_Descripton.toPlainText()})
+                self.to_do_dict.update({num: self.textEdit_Descripton.toPlainText().replace('\n', '\n    ')})
                 self.update_list()
             else:
                 self.label_error.setText(f"ERROR: Item number {str(num)}\nis not on the list")
 
     def del_item(self):
+        """
+        method deletes item number (which is from self.spinBox_itemNum) from self.to_do_dict
+        """
         try:
             num = self.spinBox_itemNum.value()
             if num == 0:
@@ -60,42 +103,23 @@ class Controller(QMainWindow, Ui_MainWindow):
             self.label_error.setText(f"ERROR: There are no items\nto delete.")
 
     def clear_list(self):
+        """
+        method clears self.to_do_dict and self.listWidget.
+        """
         self.to_do_dict.clear()
+        self.temp_to_do_dict.clear()
+        save_list(self.temp_to_do_dict)
         self.listWidget.clear()
 
     def update_list(self):
+        """
+        method updates self.listWidget from self.to_do_dict (reads the dictionary and shows it on the list widget).
+        """
         self.listWidget.clear()
         self.textEdit_Descripton.clear()
         for num in range(1, len(self.to_do_dict) + 1):
             self.listWidget.addItem(f'{str(num)}. {self.to_do_dict[num]}')
-        self.save_list()
-        self.label_error.setText("Note:\"Add Item\"\\\"Delete\nItem\" adds\\deletes the item\nat the end of the list when\n\"Item Number\" is set to 0.")
-
-    def save_list(self):
-        with open('to_do.csv', 'w') as csvfile:
-            for key in self.to_do_dict.keys():
-                csvfile.write("%s,%s\n" % (key, self.to_do_dict[key]))
-
-    def read_list(self):
-        try:
-            with open("to_do.csv", "r") as file_dict:
-                csvFile = csv.reader(file_dict)
-                for lines in csvFile:
-                    key = lines[0]
-                    value = lines[1]
-                    self.to_do_dict.update({int(key): value})
-            self.update_list()
-        except FileNotFoundError:
-            pass
-
-
-        """self.textEdit_Descripton.keyPressEvent = self.keyPressEvent
-
-    def keyPressEvent(self, e):
-        print("event", e)
-        if e.key() == Qt.Key_Return:
-            self.add_item()
-        elif e.key() == Qt.Key_Enter:
-            self.add_item()
-        else:
-            self.textEdit_Descripton.append()"""
+        self.temp_to_do_dict.update(self.to_do_dict)
+        save_list(self.temp_to_do_dict)
+        self.label_error.setText("Note:\"Add Item\"\\\"Delete\nItem\" adds\\deletes the item\nat the end of the list "
+                                 "when\n\"Item Number\" is set to 0.")
